3bc86d6f55bec01b31f44046b4c217a0801e29bb
Dan Tatar <dan.tatar@yahoo.com>
2018-09-01 16:36:10 +0100

    Reducing padding to 0 for beautyCategoriesImageView for left and right margins.

--- Beayty Addict/Views/CategoriesCell.swift
+++ Beayty Addict/Views/CategoriesCell.swift
@@ -17,18 +17,11 @@ class CategoriesCell: UITableViewCell {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.clipsToBounds = true
-//        iv.image = UIImage(named: "mascara")
+//        iv.layer.cornerRadius = 10
         iv.contentMode = .scaleAspectFill
         return iv
     }()
-//    let separatorLiveView: UIView = {
-//        let lineView = UIView()
-//        lineView.backgroundColor = UIColor(red: 230.0/255, green: 230.0/255, blue: 230.0/255, alpha: 1.0)
-//        lineView.translatesAutoresizingMaskIntoConstraints = false
-//        lineView.clipsToBounds = true
-//        lineView.isHidden = false
-//        return lineView
-//    }()
+
 
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -37,16 +30,6 @@ class CategoriesCell: UITableViewCell {
 
         backgroundColor = .white
     }
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setupViews()
-//      
-//        backgroundColor = .white
-//        
-//    }
-
-    
-    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -56,19 +39,13 @@ class CategoriesCell: UITableViewCell {
     }
     func setupViews() {
           addSubview(beautyCategoriesImageView)
-//        addSubview(separatorLiveView)
+        
         beautyCategoriesImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12).isActive = true
-        beautyCategoriesImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12).isActive = true
-         beautyCategoriesImageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12).isActive = true
+        beautyCategoriesImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
+         beautyCategoriesImageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
          beautyCategoriesImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12).isActive = true
         
-//        //lineSeparator
-//        separatorLiveView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
-//        separatorLiveView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
-//        separatorLiveView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
-//
-//        separatorLiveView.heightAnchor.constraint(equalToConstant: 1).isActive = true
-//
+
     }
     
     
